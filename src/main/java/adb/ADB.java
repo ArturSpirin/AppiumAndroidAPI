03d53c48d73c5d7290d11785a4ca979a8feab97e
Artur Spirin <a.spirin@anchorfree.com>
2016-01-20 22:45:45 -0800

    Added basic logic to most of ADB class's methods

--- src/main/java/adb/ADB.java
+++ src/main/java/adb/ADB.java
@@ -1,76 +1,87 @@
 package adb;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Created by Artur Spirin on 1/20/16.
  **/
 public class ADB {
 
-    public static String runCommand(String command){
-        return "Run Command: "+command;
+    public static String runCommand(String command) {
+        try{
+            Scanner s = new Scanner(Runtime.getRuntime().exec(command).getInputStream()).useDelimiter("\\A");
+            return s.next();
+        }catch (IOException e){
+            throw new RuntimeException("Failed to run command: "+command+"! Caught exception: "+e.getMessage());
+        }
     }
 
     public static String[] getConnectedDevices(){
         return new String[0];
     }
 
-    public static String getAppVersionAsString(String appPackage){
-        return "4.2.1";
+    public static String getAppVersionAsString(String appPackage, String deviceID) {
+        String output = runCommand("adb -s "+deviceID+" shell dumpsys package "+appPackage+" | grep -i versioncode");
+        if(output.contains("versionCode=")) output = output.replace("versionCode=", "");
+        return output;
     }
 
-    public static int getAppVersionAsInt(String appPackage){
-        return 421;
+    public static int getAppVersionAsInt(String appPackage, String deviceID){
+        return Integer.parseInt(getAppVersionAsString(appPackage, deviceID).replaceAll("\\.","").trim());
     }
 
-    public static String getAppName(String appPackage){
-        return "appName";
+    public static String getAppName(String appPackage, String deviceID) {
+        String output = runCommand("adb -s "+deviceID+" shell dumpsys package "+appPackage+" | grep -i versionname");
+        if(output.contains("versionName=")) output = output.replace("versionName=", "");
+        return output;
     }
 
-    public static int getForegroundActivity(String deviceID){
-        return 1;
+    public static String getForegroundActivity(String deviceID) {
+        return runCommand("adb -s "+deviceID+" shell dumpsys window windows | grep mCurrentFocus");
     }
 
     public static String getAndroidVersionAsString(String deviceID){
-        return "5.1.1";
+        return runCommand("adb -s "+deviceID+" shell getprop ro.build.version.release");
     }
 
     public static int getAndroidVersionAsInt(String deviceID){
-        return 511;
+        return Integer.parseInt(getAndroidVersionAsString(deviceID).replaceAll("\\.","").trim());
     }
 
     public static HashMap getInstalledPackages(String deviceID){
         return new HashMap();
     }
 
-    public static void openAppsActivity(String packageID, String activityID){
-
+    public static void openAppsActivity(String packageID, String activityID, String deviceID){
+        runCommand("adb -s "+deviceID+" shell am start -c android.intent.category.LAUNCHER -a android.intent.action.MAIN -n " +packageID+"/"+activityID);
     }
 
     public static void clearAppsData(String packageID){
-
+        runCommand("adb shell pm clear "+packageID);
     }
 
-    public static void forceStopApp(String packageID){
-
+    public static void forceStopApp(String packageID, String deviceID){
+        runCommand("adb -s "+deviceID+" shell am force-stop " + packageID);
     }
 
-    public static void installApp(String packageID){
-
+    public static void installApp(String apk, String deviceID){
+        runCommand("adb -s "+deviceID+" install "+apk);
     }
 
-    public static void uninstallApp(String packageID){
-
+    public static void uninstallApp(String packageID, String deviceID){
+        runCommand("adb -s "+deviceID+" uninstall "+packageID);
     }
 
-    public static void clearLogBuffer(String packageID){
-
+    public static void clearLogBuffer(String deviceID) {
+        runCommand("adb -s "+deviceID+" logcat -c");
     }
 
 
 
     public static String getDevicesModel(String deviceID){
-        return "model";
+        return runCommand("adb -s "+deviceID+" shell getprop ro.product.model");
     }
 
     public static String getDevicesModelID(String deviceID){
@@ -78,7 +89,7 @@ public class ADB {
     }
 
     public static String getDevicesSerialNumber(String deviceID){
-        return "deviceSerial";
+        return runCommand("adb -s "+deviceID+" shell getprop ro.serialno");
     }
 
     public static String getDevicesLanguageSettings(String deviceID){
@@ -86,27 +97,27 @@ public class ADB {
     }
 
     public static String getDevicesManufacturer(String deviceID){
-        return "manufacturer";
+        return runCommand("adb -s "+deviceID+" shell getprop ro.product.manufacturer");
     }
 
-    public static String getDevicesCarrier(){
-        return "carrier";
+    public static String getDevicesCarrier(String deviceID) {
+        return runCommand("adb -s "+deviceID+" shell getprop gsm.operator.alpha");
     }
 
     public static boolean rebootDevice(String deviceID, int secondsToWaitForBootup){
         return true; //Once device booted up
     }
 
-    public static void rebootDevice(String deviceID){
-
+    public static void rebootDevice(String deviceID) {
+        runCommand("adb -s "+deviceID+" reboot");
     }
 
-    public static String getAnrFileLocation(String deviceID){
-        return "/etc/data/anr";
+    public static String getAnrFileLocation(String deviceID) {
+        return runCommand("adb -s "+deviceID+" shell getprop dalvik.vm.stack-trace-file");
     }
 
-    public static int getDevicesWiFiMode(String deviceID){
-        return 1;
+    public static int getDevicesWiFiMode(String deviceID) {
+        return Integer.parseInt(runCommand("adb -s "+deviceID+" shell settings get global wifi_on"));
     }
 
     public static int getDevicesAirPlaneMode(String deviceID){
---
+++ src/test/java/adb/ADBTest.java
@@ -0,0 +1,121 @@
+package adb;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by a.spirin on 1/20/16.
+ */
+public class ADBTest extends TestCase {
+
+    public void testRunCommand() throws Exception {
+        System.out.println(ADB.runCommand("adb devices"));
+    }
+
+    public void testGetConnectedDevices() throws Exception {
+
+    }
+
+    public void testGetAppVersionAsString() throws Exception {
+
+    }
+
+    public void testGetAppVersionAsInt() throws Exception {
+
+    }
+
+    public void testGetAppName() throws Exception {
+
+    }
+
+    public void testGetForegroundActivity() throws Exception {
+
+    }
+
+    public void testGetAndroidVersionAsString() throws Exception {
+
+    }
+
+    public void testGetAndroidVersionAsInt() throws Exception {
+
+    }
+
+    public void testGetInstalledPackages() throws Exception {
+
+    }
+
+    public void testOpenAppsActivity() throws Exception {
+
+    }
+
+    public void testClearAppsData() throws Exception {
+
+    }
+
+    public void testForceStopApp() throws Exception {
+
+    }
+
+    public void testInstallApp() throws Exception {
+
+    }
+
+    public void testUninstallApp() throws Exception {
+
+    }
+
+    public void testClearLogBuffer() throws Exception {
+
+    }
+
+    public void testGetDevicesModel() throws Exception {
+
+    }
+
+    public void testGetDevicesModelID() throws Exception {
+
+    }
+
+    public void testGetDevicesSerialNumber() throws Exception {
+
+    }
+
+    public void testGetDevicesLanguageSettings() throws Exception {
+
+    }
+
+    public void testGetDevicesManufacturer() throws Exception {
+
+    }
+
+    public void testGetDevicesCarrier() throws Exception {
+
+    }
+
+    public void testRebootDevice() throws Exception {
+
+    }
+
+    public void testRebootDevice1() throws Exception {
+
+    }
+
+    public void testGetAnrFileLocation() throws Exception {
+
+    }
+
+    public void testGetDevicesWiFiMode() throws Exception {
+
+    }
+
+    public void testGetDevicesAirPlaneMode() throws Exception {
+
+    }
+
+    public void testGetDevicesCellMode() throws Exception {
+
+    }
+
+    public void testGetDevicesNetworkInterfaceStatus() throws Exception {
+
+    }
+}
